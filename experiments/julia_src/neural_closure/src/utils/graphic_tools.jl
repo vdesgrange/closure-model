198e035b7dd1bdcc6af3fbba084f22e638e1208b
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-05-21 19:10:47 +0200

    Update project dependencies. Update graphic utilities.

--- experiments/julia_src/neural_closure/src/utils/graphic_tools.jl
+++ experiments/julia_src/neural_closure/src/utils/graphic_tools.jl
@@ -1,8 +1,42 @@
 module GraphicTools
 
-using PyCall
-pushfirst!(pyimport("sys")."path", "")
+using Plots
+pyplot()
 
-utils = pyimport("graphic_tools.py")
+
+function simple_plotter(ks, title="Simple plot", L=1.)
+  plot()
+  for k in ks
+    x = LinRange(0, L, size(k, 1))
+    plot!(x, k)
+  end
+end
+
+function animate_plot(u, x)
+end
+
+function show_state(u, title, x="t", y="x")
+    heatmap(
+        u,
+        aspect_ratio = :equal,
+        # xlims = (1 / 2, size(u, 2) + 1 / 2),
+        # ylims = (1 / 2, size(u, 1) + 1 / 2),
+    )
+    heatmap!(
+        #xlabel = x,
+        #ylabel = y,
+        plot_title = title,
+        dpi=200,
+    )
+end
+
+function show_err(a, title, x="x", y="t", lim=none, aspect=1)
+end
+
+function domain_curve(k, v, L)
+end
+
+function visualize_u_from_F(F, t, u, u0)
+end
 
 end
