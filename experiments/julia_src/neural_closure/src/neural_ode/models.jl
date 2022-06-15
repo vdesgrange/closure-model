3f30423e0b3a42e7c31c3301222c56216ab63a3e
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-06-15 15:01:27 +0200

    Move files and update models.

--- experiments/julia_src/neural_closure/src/neural_ode/models.jl
+++ experiments/julia_src/neural_closure/src/neural_ode/models.jl
@@ -17,12 +17,28 @@ function BasicFNN(x_n)
   )
 end
 
-function BasicAutoEncoder(x_n)
+function FastBasicAutoEncoder(x_n)
   return FastChain(
     DiffEqFlux.FastDense(x_n, round(Int64, x_n / 2), tanh; bias=true, initW=Flux.kaiming_uniform, initb = Flux.zeros32),
     DiffEqFlux.FastDense(round(Int64, x_n / 2), round(Int64, x_n / 4), tanh; bias=true, initW=Flux.kaiming_uniform, initb = Flux.zeros32),
     DiffEqFlux.FastDense(round(Int64, x_n / 4), round(Int64, x_n / 2), tanh; bias=true, initW=Flux.kaiming_uniform, initb = Flux.zeros32),
     DiffEqFlux.FastDense(round(Int64, x_n / 2), x_n, identity; bias=true, initW=Flux.kaiming_uniform, initb = Flux.zeros32),
+  )
+end
+
+
+function LinearModel(x_n)
+  return Flux.Chain(
+    Flux.Dense(x_n, x_n, identity; bias=false, init=Flux.zeros32)
+  );
+end
+
+function BasicAutoEncoder(x_n)
+  return Flux.Chain(
+    Flux.Dense(x_n, round(Int64, x_n / 2), tanh; bias=true, init=Flux.kaiming_uniform),
+    Flux.Dense(round(Int64, x_n / 2), round(Int64, x_n / 4), tanh; bias=true, init=Flux.kaiming_uniform),
+    Flux.Dense(round(Int64, x_n / 4), round(Int64, x_n / 2), tanh; bias=true, init=Flux.kaiming_uniform),
+    Flux.Dense(round(Int64, x_n / 2), x_n, identity; bias=true, init=Flux.kaiming_uniform),
   )
 end
 
