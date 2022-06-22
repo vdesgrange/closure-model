3d6961181df70f8a39b8d8c0216c45a19dcf39bc
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-06-23 00:08:30 +0200

    Update objectives, add L2.

--- experiments/julia_src/neural_closure/src/neural_ode/objectives.jl
+++ experiments/julia_src/neural_closure/src/neural_ode/objectives.jl
@@ -1,7 +1,12 @@
 module Objectives
 
-function l2loss(u_pred, u)
-  return sqrt(sum(abs2, u_pred .- u) .^ 2)
+function l2(ŷ, y)
+  _check_sizes(ŷ, y)
+  sqrt(sum(abs2, ŷ .- y) .^ 2)
+end
+
+function energy(ŷ, y)
+  _check_sizes(ŷ, y)
 end
 
 end
