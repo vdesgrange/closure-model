2c9948eb5bc37f6d346d9cb93e4f50e5c1d9849e
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-07-21 20:58:15 +0200

    Update tuning file.

--- experiments/julia_src/neural_closure/src/examples/burgers/tuning.jl
+++ experiments/julia_src/neural_closure/src/examples/burgers/tuning.jl
@@ -2,7 +2,7 @@ module HO
 
 using Hyperopt
 using Hyperopt: @hyperopt
-using BSON: @save 
+using BSON: @save
 
 include("../../utils/generators.jl")
 include("../../neural_ode/models.jl")
@@ -10,11 +10,11 @@ include("./burgers_direct.jl")
 
 function f_autoencoder(i, b, r, n)
     x_n = 64;
-    epochs = 10;
+    epochs = 100;
     data = Generator.read_dataset("./dataset/burgers_high_dim_training_set.jld2")["training_set"];
     model = Models.BasicAutoEncoder(x_n);
     K, p, l_train, _ = BurgersDirect.training(model, epochs, data, b, 0.5, n, r, true);
-   
+
     filename = "./models/tuning_burgers_basicautoencoder_" * string(i) * ".bson"
     @save filename K p
 
@@ -27,10 +27,10 @@ function tuning(n_iteration)
             sampler = Hyperopt.RandomSampler(),
             b = [128, 64, 32],
             r = [1f-8, 1f-6, 1f-5, 1f-4, 1f-3, 1f-2, 1f-1],
-            n = [.3, .2, .15, .1, .05, .01]
+            n = [.3, .25, .2, .15, .1, .05, .01]
         l = f_autoencoder(i, b, r, n);
-        @show l
-        push!(logs, string(i, "\t", b, "\t", r, "\t", n, "   \t f_autoencoder(b, r, n) = ", l, "    \t"));
+        @show "\t", i, "\t", b, "\t", r, "\t", n, "   \t f_autoencoder(b, r, n) = ", l, "    \t"
+        push!(logs, string(i, " ", b, " ", r, " ", n, "     f_autoencoder(b, r, n) = ", l, "  "));
     end
 
     for log in logs
