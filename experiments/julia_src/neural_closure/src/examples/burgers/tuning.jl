6734f10effaeee00d57f0832fd1577e3667b19b7
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-07-23 23:43:15 +0200

    Update tuning and package.

--- experiments/julia_src/neural_closure/src/examples/burgers/tuning.jl
+++ experiments/julia_src/neural_closure/src/examples/burgers/tuning.jl
@@ -20,25 +20,23 @@ pids = addprocs(4; exeflags=`--project=$(Base.active_project())`);
 
     data = Generator.read_dataset("./dataset/burgers_high_dim_training_set.jld2")["training_set"];
     model = Models.BasicAutoEncoder(x_n);
-    K, p, l_train, _ = BurgersDirect.training(model, epochs, data, b, lr, ratio, n, r, true);
+    K, p, l_train, _ = BurgersDirect.training(model, epochs, data, b, lr, ratio, n, r, false);
 
-    filename = "./models/tuning_burgers_basicautoencoder_2_" * string(i) * ".bson"
+    filename = "./models/tuning_burgers_basicautoencoder_2_worker_" * string(myid()) * "_iter_" * string(i) * ".bson"
     @save filename K p
 
     return l_train
 end
 
-if nprocs() > 1
-    ho = @phyperopt for i = 50,
-        sampler = RandomSampler(),
-            lr = LinRange(1f-4, 1f-1, 8),
-            b = [128, 64, 32, 16],
-            r = [1f-8, 1f-7, 1f-6, 1f-5, 1f-4, 1f-3, 1f-2, 1f-1],
-            n = [.35, .3, .25, .2, .15, .1, .05, .01]
-        l = f_autoencoder(i, lr, b, r, n);
-        print(i, "\t", b, "\t", r, "\t", n, "   \t f_autoencoder(b, r, n) = ", l, "    \t")
-        @show l
-    end
+ho = @phyperopt for i = 50,
+    sampler = RandomSampler(),
+        lr = LinRange(1f-4, 1f-1, 8),
+        b = [128, 64, 32, 16],
+        r = [1f-8, 1f-7, 1f-6, 1f-5, 1f-4, 1f-3, 1f-2, 1f-1],
+        n = [.35, .3, .25, .2, .15, .1, .05, .01]
+    l = f_autoencoder(i, lr, b, r, n);
+    print(i, "\t", b, "\t", r, "\t", n, "   \t f_autoencoder(b, r, n) = ", l, "    \t")
+    @show l
 end
 
 JLD2.save("hyperopt_result.jld2", "ho", ho);
