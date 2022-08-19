e2f51032a0d49df6f94fa5e1e6cd0a9ef2432877
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-08-19 13:23:10 +0200

    Add additional nn architectures.

--- experiments/julia_src/neural_closure/src/neural_ode/models.jl
+++ experiments/julia_src/neural_closure/src/neural_ode/models.jl
@@ -96,9 +96,35 @@ function CAE(x_n, l, k)
   end
 
   return Flux.Chain(
-    encoder,
-    decoder,
+    Flux.Dense(x_n => x_n, tanh; init=Flux.glorot_uniform, bias=true),
+    encoder...,
+    decoder...,
     Flux.Dense(x_n => x_n, identity; init=Flux.glorot_uniform, bias=true),
   )
 end
+
+function CAE2(k)
+  encoder = [
+    Flux.Conv((k, 1), 1 => 4, sigmoid; stride = 2, dilation = 2, pad = SamePad(), bias=true, init=Flux.glorot_uniform),
+    Flux.Conv((k, 1), 4 => 8, sigmoid; stride = 2, dilation = 2, pad = SamePad(), bias=true, init=Flux.glorot_uniform),
+    Flux.Conv((k, 1), 8 => 16, sigmoid; stride = 2, dilation = 2, pad = SamePad(), bias=true, init=Flux.glorot_uniform),
+  ];
+
+  decoder = [
+    Flux.ConvTranspose((k, 1), 16 => 8, sigmoid; stride = (2, 1), dilation = 1, pad = SamePad(), bias=true, init=Flux.glorot_uniform),
+    Flux.ConvTranspose((k, 1), 8 => 4, sigmoid; stride = (2, 1), dilation = 1, pad = SamePad(), bias=true, init=Flux.glorot_uniform),
+    Flux.ConvTranspose((k, 1), 4 => 1, identity; stride = (2, 1), dilation = 1, pad = SamePad(), bias=true, init=Flux.glorot_uniform),
+  ];
+
+  return Flux.Chain(
+    encoder...,
+    # x -> reshape(x, :, size(x, 4)),
+    # Flux.Dense(128 => 64, sigmoid; init=Flux.glorot_uniform, bias=true),
+    # Flux.Dense(64 => 64, sigmoid; init=Flux.glorot_uniform, bias=true),
+    # Flux.Dense(64 => 128, sigmoid; init=Flux.glorot_uniform, bias=true),
+    # x -> reshape(x, 8, 1, 16, size(x, 2)),
+    decoder...,
+  )
+end
+
 end
