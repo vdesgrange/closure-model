597b8800bd0a6f3ba1246e9fb9204cdbe1abb3cd
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-10-07 15:54:33 +0200

    Update train file. Add datasets.

--- experiments/julia_src/neural_closure/src/examples/burgers/train.jl
+++ experiments/julia_src/neural_closure/src/examples/burgers/train.jl
@@ -141,7 +141,7 @@ end
 
 function main6()
   x_n = 64; # Discretization
-  epochs = 200; # Iterations
+  epochs = 500; # Iterations
   ratio = 0.75; # train/val ratio
   lr = 0.003; # learning rate
   reg = 1e-7; # weigh decay (L2 reg)
@@ -149,11 +149,30 @@ function main6()
   batch = 3;
 
   opt = OptimizationOptimisers.ADAMW(lr, (0.9, 0.999), reg);
-  data = Generator.read_dataset("./dataset/inviscid_burgers_advecting_shock_t2_4_j173.jld2")["training_set"];
+  data = Generator.read_dataset("./dataset/inviscid_burgers_advecting_shock_nu0001_t2_4_j173.jld2")["training_set"];
   model = Models.CNN2(9, [2, 4, 8, 8, 4, 2, 1]);
   K, p, _ = BurgersCombinedCNN.training(model, epochs, data, opt, batch, ratio, n, Tsit5());
-  @save "./models/inviscid_burgers_advecting_shock_t2_4_j173.bson" K p
+  @save "./models/inviscid_burgers_advecting_shock_nu0001_t2_4_j173.bson" K p
 
   return K, p
 end
 
+
+function main7()
+  x_n = 64; # Discretization
+  epochs = 500; # Iterations
+  ratio = 0.75; # train/val ratio
+  lr = 0.003; # learning rate
+  reg = 1e-7; # weigh decay (L2 reg)
+  n = 0.; # noise
+  batch = 16;
+
+  opt = OptimizationOptimisers.ADAMW(lr, (0.9, 0.999), reg);
+  
+  data = Generator.read_dataset("./dataset/inviscid_burgers_high_dim_m10_256_t2_j173.jld2")["training_set"];
+  model = Models.CNN2(9, [2, 4, 8, 8, 4, 2, 1]);
+  K, p, _ = BurgersCombinedCNN.training(model, epochs, data, opt, batch, ratio, n, Tsit5());
+  @save "./models/inviscid_burgers_high_dim_m10_256_t2_j173.bson" K p
+
+  return K, p
+end
