f051a9fcea8461e4d57d59af00de217f809e5f19
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-10-10 16:50:11 +0200

    MERDE

--- experiments/julia_src/neural_closure/src/examples/burgers/burgers_closure.jl
+++ experiments/julia_src/neural_closure/src/examples/burgers/burgers_closure.jl
@@ -1,16 +1,42 @@
 # module BurgersClosure
-
+using FFTW
+using AbstractFFTs
+using Statistics
+using Zygote
+using Flux
+using DiffEqFlux
+using OrdinaryDiffEq
+using DiffEqSensitivity
+using Optimization
+using OptimizationOptimisers
+using IterTools: ncycle
 using CUDA
 using BSON: @save
-using Flux
-using OrdinaryDiffEq
-using DiffEqFlux
-using Optimization
-using IterTools: ncycle
 
+include("../../neural_ode/models.jl")
 include("../../utils/processing_tools.jl")
 include("../../neural_ode/regularization.jl")
 include("../../rom/pod.jl")
+include("../../utils/generators.jl")
+include("../..//utils/graphic_tools.jl")
+
+x_n = 64; # Discretization
+epochs = 50; # Iterations
+ratio = 0.75; # train/val ratio
+lr = 0.003; # learning rate
+reg = 1e-7; # weigh decay (L2 reg)
+noise = 0.; # noise
+batch_size = 3;
+sol = Tsit5();
+cuda = false;
+ν = 0.001;
+
+opt = OptimizationOptimisers.ADAMW(lr, (0.9, 0.999), reg);
+dataset = Generator.read_dataset("./dataset/inviscid_burgers_advecting_shock_nu0001_t2_4_j173.jld2")["training_set"];
+# dataset = Generator.read_dataset("./dataset/inviscid_burgers_high_dim_m10_256_j173.jld2")["training_set"];
+model = Models.CNN2(9, [2, 2, 1]);
+
+# =======
 
 del_dim(x::Array) = reshape(x, (size(x)[1], size(x)[3], size(x)[4]))
 
@@ -30,44 +56,6 @@ function f(u, p, t)
   return real.(uₜ)
 end
 
-function f_pod(u, p, t)
-  k, ν, Φ  = p[1], p[2], p[3];
-  u_pod = Φ * Φ' * f(u, (k, ν), t)
-end
-
-# =======
-
-using FFTW
-using AbstractFFTs
-using OrdinaryDiffEq
-using DiffEqSensitivity
-using Statistics
-using Zygote
-using OrdinaryDiffEq: Tsit5
-using OptimizationOptimisers
-include("../../neural_ode/models.jl");
-include("../../utils/generators.jl");
-include("./burgers_cnn.jl");
-include("./burgers_combined_optimizer.jl");
-
-x_n = 64; # Discretization
-epochs = 100; # Iterations
-ratio = 0.75; # train/val ratio
-lr = 0.003; # learning rate
-reg = 1e-7; # weigh decay (L2 reg)
-noise = 0.; # noise
-batch_size = 3;
-sol = Tsit5();
-cuda = false;
-ν = 0.04;
-
-opt = OptimizationOptimisers.ADAMW(lr, (0.9, 0.999), reg);
-dataset = Generator.read_dataset("./dataset/inviscid_burgers_advecting_shock_nu0001_t2_4_j173.jld2")["training_set"];
-model = Models.CNN2(9, [2, 4, 8, 8, 4, 2, 1]);
-# K, p, _ = BurgersCombinedCNN.training(model, epochs, dataset, opt, batch_size, ratio, noise, sol);
-#@save "./models/inviscid_burgers_advecting_shock_t2_4_j173.bson" K p
-
-# =======
 
 # function training(model, epochs, dataset, opt, batch_size, ratio, noise=0., sol=Tsit5(), cuda=false)
    if cuda && CUDA.has_cuda()
@@ -87,14 +75,16 @@ model = Models.CNN2(9, [2, 4, 8, 8, 4, 2, 1]);
   @info("Loading dataset")
   (train_loader, val_loader) = ProcessingTools.get_data_loader_cnn(dataset, batch_size, ratio, false, cuda);
   
-  u_ref = [];
+  tmp = [];
   for (x, y, t) in train_loader
-    push!(u_ref, y);
-    push!(u_ref, y);
+    push!(tmp, y);
   end
-  print(size(reshape(Array(u_ref), size(u_ref)[1])))
-  bas, _ = POD.generate_pod_svd_basis(reshape(Array(u_ref), size(y)[1], :), false);
-  Φ = bas.modes;
+  u_ref = cat(tmp...; dims=3);
+  xₙ = size(u_ref)[1];
+  Δx = 1. / xₙ;
+  k = 2 * pi * AbstractFFTs.fftfreq(xₙ, 1. / Δx);
+  bas, _ = POD.generate_pod_svd_basis(reshape(Array(u_ref), xₙ, :), false);
+  Φ = bas.modes[:, 1:3];
 
   @info("Building model")
   model_gpu = model |> device;
@@ -102,28 +92,21 @@ model = Models.CNN2(9, [2, 4, 8, 8, 4, 2, 1]);
   p = p |> device;
   net(u, p, t) = re(p)(u);
 
-  function predict_neural_ode(θ, x, t)
-    tspan = (float(t[1]), float(t[end]));
-    _prob = ODEProblem(net, x, tspan, θ);
-    ȳ = device(solve(_prob, sol, u0=x, p=θ, abstol=1e-6, reltol=1e-6, saveat=t, sensealg=DiffEqSensitivity.BacksolveAdjoint(autojacvec=ZygoteVJP())));
-    return permutedims(del_dim(ȳ), (1, 3, 2));
-
-    # xₙ = size(x)[1];
-    # Δx = 1. / xₙ;
-    # k = 2 * pi * AbstractFFTs.fftfreq(xₙ, 1. / Δx);
-
-    # function f_NN_ϵ(u, p, t)
-    #   du = f_pod(u, (k, ν, Φ), t);
-    #   ϵ = net(u, p, t);
-    #   du + ϵ
-    # end
-
-    # _prob = ODEProblem(net, x, extrema(t), θ); # (θ, k, ν, Φ)
-    # ŷ = device(solve(_prob, sol, u0=x, p=θ, abstol=1e-9, reltol=1e-9, saveat=t));
-    # return permutedims(del_dim(ŷ), (1, 3, 2)); # del_dim(ŷ)
+  function f_NN(u, p, t)
+    net(u, p, t);
   end
 
-  #loss(p) = loss_derivative_fit(p, U, 1e-8)
+  function f_NN_ϵ(u, p, t)
+    du =  Φ * Φ' * f(u, (k, ν), t);  # du = f_pod(u, (k, ν, Φ), t);
+    ϵ = net(u, p, t);
+    du + ϵ
+  end
+
+  function predict_neural_ode(θ, x, t)
+    _prob = ODEProblem(f_NN, x, extrema(t), θ);  # (θ, k, ν, Φ)
+    ȳ = device(solve(_prob, sol, u0=x, p=θ, abstol=1e-6, reltol=1e-6, saveat=t, sensealg=DiffEqSensitivity.BacksolveAdjoint(autojacvec=ZygoteVJP())));
+    return permutedims(del_dim(ȳ), (1, 3, 2));
+  end
 
   function loss(θ, x, y, t)
     x̂ = Reg.gaussian_augment(x, noise);
@@ -133,7 +116,7 @@ model = Models.CNN2(9, [2, 4, 8, 8, 4, 2, 1]);
   end
 
   function val_loss(θ, x, y, t)
-    ŷ = predict_neural_ode(θ, x, t[1], bas.modes[:, 1:3]);
+    ŷ = predict_neural_ode(θ, x, t[1]);
     l = Flux.mse(ŷ, y)
     return l;
   end
@@ -173,4 +156,21 @@ model = Models.CNN2(9, [2, 4, 8, 8, 4, 2, 1]);
 # end
 
 K = re(result_neuralode.u)
+p  = Flux.params(K);
+
+add_dim(x::Array{Float64, 1}) = reshape(x, (size(x)[1], 1, 1))
+t, u = dataset[1];
+x = LinRange(0., 1., xₙ)
+u₀ = u[:, 1];
+prob_neuralode = DiffEqFlux.NeuralODE(K,  extrema(t), Tsit5(), saveat=t);
+u_pred = prob_neuralode(add_dim(u₀))
+GraphicTools.show_state(u, t, x, "", "t", "x")
+GraphicTools.show_state(hcat(u_pred.u...)[:, :], t, x, "", "t", "x")
+
+for (x, y, t) in val_loader
+  prob_neuralode = DiffEqFlux.NeuralODE(K,  extrema(t[1]), Tsit5(), saveat=t[1]);
+  u_pred = prob_neuralode(x)
+  display(GraphicTools.show_state(hcat(u_pred.u...)[:, :], t[1], x, "", "t", "x"))
+end
+
 #  return K, p
